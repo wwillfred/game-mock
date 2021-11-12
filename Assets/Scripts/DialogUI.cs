958f21e031d7e9c82fec8f79d54123ed0ecda680
wwillfred <93595582+wwillfred@users.noreply.github.com>
2021-11-12 06:43:14 -0500

    can close dialog box
    Created method that hides the dialog box and sets its text label to empty string

--- Assets/Scripts/DialogUI.cs
+++ Assets/Scripts/DialogUI.cs
@@ -4,6 +4,7 @@ using TMPro;
 
 public class DialogUI : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogBox;
     [SerializeField] private TMP_Text textLabel; // pointer to a text label that will be specified within the Unity editor. we will pass this label to the typewriterEffect so it knows where to write to
     [SerializeField] private DialogObject testDialog;
 
@@ -13,12 +14,14 @@ public class DialogUI : MonoBehaviour
     private void Start()
     {
         typewriterEffect = GetComponent<TypewriterEffect>(); // set the pointer to the actual class
+        CloseDialogBox(); // make sure the dialog box is not showing at beginning of game
         ShowDialog(testDialog); // sends the testDialog be typewritered
     }
 
     //public method for specifying a dialogObject (with a String array) and for "typewritering" the strings
     public void ShowDialog(DialogObject dialogObject)
     {
+        dialogBox.SetActive(true); // make the dialog box appear!
         StartCoroutine(StepThroughDialog(dialogObject)); // calls the private method for typewritering the dialog
     }
 
@@ -30,6 +33,15 @@ public class DialogUI : MonoBehaviour
             yield return typewriterEffect.Run(dialog, textLabel); // tell the typewriterEffect to do its thing with this string with the text label that we specified in Unity editor
             yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
         }
+
+        CloseDialogBox();
+    }
+
+    // private method for closing the dialog box
+    private void CloseDialogBox()
+    {
+        dialogBox.SetActive(false); // make dialog box invisible to user
+        textLabel.text = string.Empty; // make sure there's no residual text hidden in the box
     }
 }
 

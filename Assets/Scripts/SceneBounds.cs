4910ad6980a322f38f57403f4fa22ccf8f427b13
wwillfred <93595582+wwillfred@users.noreply.github.com>
2021-12-23 07:12:50 -0500

    Simplified edge detection code, added bottom edge detection
    But now unsure: should I be using the position of BoxCollider2D (if it exists?) instead of the GameObject.transform.position for the player and the scene object?

--- Assets/Scripts/SceneBounds.cs
+++ Assets/Scripts/SceneBounds.cs
@@ -15,22 +15,42 @@ public class SceneBounds : MonoBehaviour
     {
         if (other.CompareTag("Player") && other.TryGetComponent(out PlayerController player)) //does this have the Player tag and the PlayerController script attached?
         {
-            Vector2 playerPosition = other.gameObject.transform.position;
-            Vector2 playerColliderSize = other.GetComponent<BoxCollider2D>().size;
+            float playerRHEdge = other.gameObject.transform.position.x + other.GetComponent<BoxCollider2D>().size.x / 2;
+            float playerTopEdge = other.gameObject.transform.position.y + other.GetComponent<BoxCollider2D>().size.y / 2;
+            float playerLHEdge = other.gameObject.transform.position.x - other.GetComponent<BoxCollider2D>().size.x / 2;
+            float playerBottomEdge = other.gameObject.transform.position.y - other.GetComponent<BoxCollider2D>().size.y / 2;
 
-            Vector2 cameraPosition = gameObject.transform.position;
-            Vector2 cameraSize = boxCollider.size;
+            float sceneRHEdge = gameObject.transform.position.x + boxCollider.size.x / 2;
+            float sceneTopEdge = gameObject.transform.position.y + boxCollider.size.y / 2;
+            float sceneLHEdge = gameObject.transform.position.x - boxCollider.size.x / 2;
+            float sceneBottomEdge = gameObject.transform.position.y - boxCollider.size.y / 2;
 
-            Debug.Log("Player is triggering scene bounds. Player position is: " + playerPosition + ". Player size is: " + playerColliderSize + ". Camera position is: " + cameraPosition + ". Camera size is: " + cameraSize);
+            //Vector2 playerPosition = other.gameObject.transform.position;
+            //Vector2 playerColliderSize = other.GetComponent<BoxCollider2D>().size;
 
-            //is RH edge of player equal to or more than RH edge of collider, and is player between top and bottom edges of collider?
-            if (playerPosition.x + playerColliderSize.x /2 >= cameraPosition.x + cameraSize.x / 2 && playerPosition.y + playerColliderSize.y / 2 < cameraPosition.y + cameraSize.y / 2 && playerPosition.y - playerColliderSize.y /2 > cameraPosition.y - cameraSize.y /2)
+            //Vector2 cameraPosition = gameObject.transform.position;
+            //Vector2 cameraSize = boxCollider.size;
+
+            Debug.Log("Player is triggering scene bounds. Player edges are: " + playerRHEdge + ", " + playerBottomEdge + ", " + playerLHEdge + ", " + playerTopEdge + ". Scene edges are: " + sceneRHEdge + ", " + sceneBottomEdge + ", " + sceneLHEdge + ", " + sceneTopEdge);
+
+            //is RH edge of player equal to or more than RH edge of scene, and is player between top and bottom edges of scene?
+            if (playerRHEdge >= sceneRHEdge && playerTopEdge < sceneTopEdge && playerBottomEdge > sceneBottomEdge)
             {
-                Debug.Log("Player is leaving RH side of screen. Player position is: " + playerPosition + ". Player size is: " + playerColliderSize + ". Camera position is: " + cameraPosition + ". Camera size is: " + cameraSize);
+                Debug.Log("Player is leaving RH edge of scene. Player RH edge: " + playerRHEdge + ", scene RH edge: " + sceneRHEdge + ". Player top edge: " + playerTopEdge + ", scene top edge: " + sceneTopEdge + ". Player bottom edge: " + playerBottomEdge + ", scene bottom edge: " + sceneBottomEdge);
             }
-            else if (playerPosition.y + playerColliderSize.y /2 >= cameraPosition.y + cameraSize.y /2 && playerPosition.x + playerColliderSize.x / 2 < cameraPosition.x + cameraSize.x /2 && playerPosition.x - playerColliderSize.x /2 > cameraPosition.x - cameraSize.x / 2)
+            //is top edge of player equal to or more than top edge of scene, and is player between LH and RH edges of scene?
+            else if (playerTopEdge >= sceneTopEdge && playerRHEdge < sceneRHEdge && playerLHEdge > sceneLHEdge)
             {
-                Debug.Log("Player has left the top edge of screen. Player position is: " + playerPosition + ". Player size is: " + playerColliderSize + ". Camera position is: " + cameraPosition + ". Camera size is: " + cameraSize);
+                Debug.Log("Player is leaving top edge of scene. Player top edge: " + playerTopEdge + ", scene top edge: " + sceneTopEdge + ". Player LH edge: " + playerLHEdge + ", scene LH edge: " + sceneLHEdge + ". Player RH edge: " + playerRHEdge + ", scene RH edge: " + sceneRHEdge);
+            }
+            //is LH edge of player equal to or less than LH edge of scene, and is player between top and bottom edges of scene?
+            else if (playerLHEdge <= sceneLHEdge && playerTopEdge < sceneTopEdge && playerBottomEdge > sceneBottomEdge)
+            {
+                Debug.Log("Player is leaving LH edge of scene. Player LH edge: " + playerLHEdge + ", scene LH edge: " + sceneLHEdge + ". Player top edge: " + playerTopEdge + ", scene top edge: " + sceneTopEdge + ". Player bottom edge: " + playerBottomEdge + ", scene bottom edge: " + sceneBottomEdge);
+            }
+            else if (playerBottomEdge <= sceneBottomEdge && playerLHEdge > sceneLHEdge && playerRHEdge < sceneRHEdge)
+            {
+                Debug.Log("Player is leaving bottom edge of scene. Player bottom edge: " + playerBottomEdge + ", scene bottom edge: " + sceneBottomEdge + ". Player LH edge: " + playerLHEdge + ", scene LH edge: " + sceneLHEdge + ". Player RH edge: " + playerRHEdge + ", scene RH edge: " + sceneRHEdge);
             }
 
         }

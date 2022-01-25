91205d01c7e0b8e6ad4fa24f07a8059f7970d865
wwillfred <93595582+wwillfred@users.noreply.github.com>
2022-01-25 07:38:05 -0500

    Scene 3: truck and worker B now move to working positions
    It gets the job done!

--- Assets/Scripts/Interactables/QuarryWorkerAController.cs
+++ Assets/Scripts/Interactables/QuarryWorkerAController.cs
@@ -7,6 +7,10 @@ public class QuarryWorkerAController : DialogActivator
     [SerializeField] private DialogObject dialog_ReturnToQuarry;
 
     [SerializeField] private GameObject truck;
+    public Vector2 truckWorkingPosition;
+
+    [SerializeField] private GameObject quarryWorkerB;
+    public Vector2 quarryWorkerBWorkingPosition;
 
     public Vector2 workingPosition;
     public float timeToWorkingPositionX = 10;
@@ -30,6 +34,9 @@ public class QuarryWorkerAController : DialogActivator
     private IEnumerator GoBackToWork(PlayerController playerController)
     {
         yield return playerController.DialogUI.ShowDialog(dialog_ReturnToQuarry);
+
+        truck.transform.position = truckWorkingPosition;
+        quarryWorkerB.transform.position = quarryWorkerBWorkingPosition;
 
         GetComponent<BoxCollider2D>().enabled = false;
         Vector3 targetPosition = new Vector3(workingPosition.x, transform.position.y, transform.position.z);

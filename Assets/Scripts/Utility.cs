e312ea40e4d6d3dd520f8bff7cdf4883e6b10769
wwillfred <93595582+wwillfred@users.noreply.github.com>
2022-03-31 17:26:19 -0400

    Created text for player max x just greater than camera max x

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -6,7 +6,7 @@ public static class Utility
 {
     public static bool CheckPlayerIntersectingCameraMax_X(Bounds cameraBounds, Bounds playerBounds)
     {
-        Debug.Log("Running static RHBounds method. playerBoundsMaxX is: " + playerBounds.max.x + ", and cameraBoundsMaxX is: " + cameraBounds.max.x);
+        Debug.Log("Running static Max_X intersection check. playerBoundsMaxX is: " + playerBounds.max.x + ", and cameraBoundsMaxX is: " + cameraBounds.max.x);
         return (playerBounds.max.x >= cameraBounds.max.x);
     }
 }
